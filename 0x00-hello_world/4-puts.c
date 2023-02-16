e451531ec09c32d13edde6442b6df4cd8f3283bc
Godwindavid07 <davidgodwin065@gmail.com>
2023-02-16 13:17:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entery
- * Return: Always 0
+ * main - Entry
+ * Return: 0
  */
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
