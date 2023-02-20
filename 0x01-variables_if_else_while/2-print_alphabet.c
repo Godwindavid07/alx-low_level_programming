4eb0a3ef518e13d98bb0037198b4449e2d55b22e
Godwindavid07 <davidgodwin065@gmail.com>
2023-02-20 01:58:09 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	char i;
+	char alph[27] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (i = 'a'; i <= 'Z'; i++)
+	for (i = 0; i < 27; i++)
 	{
-		putchar(i);
+		putchar(alph[i]);
 	}
 			putchar('\n');
 			return (0);
