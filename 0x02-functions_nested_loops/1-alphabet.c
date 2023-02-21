ec22ca2ec8a3f51e6d34b81ef547afb0248999f9
Godwindavid07 <davidgodwin065@gmail.com>
2023-02-21 15:55:47 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
+
+int _putchar(char c)
+{
+	/* implimentation of the word char */
+}
