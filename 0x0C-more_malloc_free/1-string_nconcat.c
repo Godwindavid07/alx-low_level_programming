6dafc362bc086c1298dc78d2ab8f61e31dd6f923
Godwindavid07 <davidgodwin065@gmail.com>
2023-11-30 17:52:21 -0800

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
 /**
  * string_nconcat - concatinates two strings
  *
@@ -20,18 +20,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		a = 0;
 	else
 	{
-		for (a = 0; s1[a] != '\0' a++)
+		for (a = 0; s1[a] != '\0'; a++)
 			;
 	}
 	if (s2 == NULL)
 		b = 0;
 	else
 	{
-		for (b = 0; s2[b] != '\0' b++)
+		for (b = 0; s2[b] != '\0'; b++)
 			;
 	}
 	if (b > n)
-		b = n;
+	{
+		n = b;
+	}
 
 	ptr = malloc(sizeof(char) * (a + b + 1));
 	if (ptr == NULL)
@@ -39,9 +41,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	for (c = 0; c < a; c++)
+	{
 		ptr[c] = s1[c];
-	for (k = 0; k < b; k++)
-		ptr[k + 1] = s2[k];
-	ptr[a + j] = '\0';
+	}
+	for (c = 0; c < b; c++)
+	{
+		ptr[c + 1] = s2[c];
+	}
+	ptr[a + b] = '\0';
 	return (ptr);
 }
